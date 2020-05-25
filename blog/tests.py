05ba8c029835240500e28833f11caa196b9a573b
naeglinghaff <kate.o.haffenden@gmail.com>
2020-05-25 17:36:55 +0100

    Updated tests for post_edit view function, is_authenticated currently raising error

--- blog/tests.py
+++ blog/tests.py
@@ -1,13 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.test import Client
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from .models import Post
+from .views import post_edit
 
 class TestBlogViews(TestCase):
     def setUp(self):
         client = Client()
-        author = User(id = 1, is_superuser = 1, username = "naeglinghaff").save()
-        AUTH_USER_MODEL = author
+        self.factory = RequestFactory()
+        self.user = User(id = 1, is_superuser = 1, username = "naeglinghaff", password = "fakepassword").save()
         post = Post(title = 'My Post', text = 'This is some text', author_id = 1).save()
 
     def test_homepage_can_render_posts(self):
@@ -21,3 +23,15 @@ class TestBlogViews(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "blog/post_detail.html")
+
+    def test_blog_edit_view_can_render(self):
+        request = self.factory.get("/post/1/edit/")
+
+        self.user = authenticate(username = "naeglinghaff", password = "fakepassword")
+
+        request.user = self.user
+
+        response = post_edit(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "blog/post_edit.html")
