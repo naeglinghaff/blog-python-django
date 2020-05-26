ee2ff3f1402912f9f5c5e88f0ced1c8d422433e6
naeglinghaff <kate.o.haffenden@gmail.com>
2020-05-26 16:01:16 +0100

    Updated test names, added test for post_remove view

--- blog/tests.py
+++ blog/tests.py
@@ -3,7 +3,7 @@ from django.test import Client
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from .models import Post
-from .views import post_edit, post_new
+from .views import post_edit, post_new, post_remove
 
 class TestBlogViews(TestCase):
     def setUp(self):
@@ -14,19 +14,19 @@ class TestBlogViews(TestCase):
         self.user.save()
         self.post = Post(title = 'My Post', text = 'This is some text', author_id = 1).save()
 
-    def test_homepage_can_render_posts(self):
+    def test_homepage_post_list_can_render_posts_(self):
         response = self.client.get("//")
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "blog/post_list.html")
 
-    def test_blog_detail_view_can_render(self):
+    def test_blog__post_detail_view_can_render(self):
         response = self.client.get("/post/1/")
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "blog/post_detail.html")
 
-    def test_blog_edit_view_can_render(self):
+    def test_blog_post_edit_view_returns_200(self):
         request = self.factory.get("/post/1/edit/")
         self.client.login(username="naeglinghaff", password="12345")
         request.user = self.user
@@ -35,7 +35,7 @@ class TestBlogViews(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
-    def test_blog_edit_view_can_render(self):
+    def test_blog_post_new_view_returns_200(self):
         request = self.factory.get("/post/new/")
         self.client.login(username="naeglinghaff", password="12345")
         request.user = self.user
@@ -43,3 +43,12 @@ class TestBlogViews(TestCase):
         response = post_new(request)
 
         self.assertEqual(response.status_code, 200)
+
+    def test_blog_post_remove_view_returns_302(self):
+        request = self.factory.get("/post/1/remove/")
+        self.client.login(username="naeglinghaff", password="12345")
+        request.user = self.user
+
+        response = post_remove(request, pk = 1)
+
+        self.assertEqual(response.status_code, 302)
