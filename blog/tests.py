b534ad5fccfc4d7abdc10065b5022cd8ec7c2efa
naeglinghaff <kate.o.haffenden@gmail.com>
2020-05-28 15:12:33 +0100

    Added test for unapproved comments not being returned - approved_comments function

--- blog/tests.py
+++ blog/tests.py
@@ -107,8 +107,14 @@ class TestBlogPostModel(TestCase):
         comment = Comment(id = 1, author = 1, post_id = 1, text="this is a comment", approved_comment = True)
         comment.save()
         result = post.approved_comments()
-        print(result)
         self.assertEqual(result[0], comment)
+
+    def test_post_model_does_not_fetch_unapproved_comments(self):
+        post = Post(title="hello", text="This is some text", author_id = 1)
+        post.publish()
+        comment = Comment(id = 1, author = 1, post_id = 1, text="this is a comment")
+        comment.save()
+        self.assertEqual(post.approved_comments().count(), 0)
 
     def test_post_model_publish_save_to_db_save_timestamp(self):
         with patch.object(timezone, 'now', return_value=self.timezone.localize(datetime.datetime(2020, 1, 1, 11, 00))) as mock_now:
