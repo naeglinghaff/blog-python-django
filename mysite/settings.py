470fde54912752d4cc9b4bef7a05ad244fb250c8
naeglinghaff <kate.o.haffenden@gmail.com>
2020-06-16 16:27:16 +0100

    Removed cache from settings for static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -153,6 +153,4 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 django_heroku.settings(locals())
